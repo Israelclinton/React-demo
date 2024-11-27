360a8a0050329989f49dcb4457b6751fb70ab68c
Tristan98943 <blossomchristopher9@gmail.com>
2024-11-27 16:16:01 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,142 @@
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import React from 'react'
+
+// const App = () => {
+//   const name = 'John';
+//   const x = 10;
+//   const y = 20;
+//   const names = ['John', 'Isreal', 'Joe', 'Sara'];
+//   const loggedIn = true;
+//   // const loggedIn = false;
+//   const styles = {
+//     color: 'red',
+//     fontSize: '55px',
+//   }
+
+//   // if (loggedIn) {
+//   //   return <h1>Hello Member</h1>;
+//   // }
+
+//   return (
+//     <>
+//     <div className='text-5xl'>App</div>
+//     {/* <p style = {{color: 'red', fontSize: '24px'}} >Hello {name} </p> */}
+//     <p style = {styles} >Hello {name} </p>
+//     <p>The sum of {x} and {y} is {x + y} </p>
+//     <ul className="list-decimal list-style-type: decimal;">
+//       {names.map((name, index) => (
+//         <li key={index}>{name}</li>
+//       ))}
+//     </ul>
+//     { loggedIn && <h1>Hello Member</h1> }
+//     {/* { loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1> } */}
+//     </>
+//   );
+// };
+
+// export default App;
